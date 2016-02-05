703e0b12da44a2098d11b61207050d2bc5352c77
Leandro Passos <lpkyrius@hotmail.com>
2016-02-05 10:37:46 -0200

    Entity

---
+++ Quiron.LojaVirtual.Dominio/Repositorio/EfDbContext.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using Quiron.LojaVirtual.Dominio.Entidades; // Exigiu esta declaração para reconhecer DbSet<Produto>
+
+namespace Quiron.LojaVirtual.Dominio.Repositorio
+{
+    public class EfDbContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // Remove a pluralidade que faria com que o Entity procurasse automaticamente o nome da tabela conforme o nome do DbSet acima. No caso Produtos
+            modelBuilder.Conventions.Remove<System.Data.Entity.ModelConfiguration.Conventions.PluralizingTableNameConvention>();
+            
+            // Uma vez removida a pluralidade, agora eu mesmo informo qual o nome da tabela no banco
+            modelBuilder.Entity<Produto>().ToTable("Produtos");
+        }
+
+    }
+}
---
+++ Quiron.LojaVirtual.Web/Controllers/ProdutosController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Quiron.LojaVirtual.Web.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        private Dominio.Repositorio.ProdutosRepositorio _repositorio;
+
+        // GET: Produtos
+        public ActionResult Index()
+        {
+            _repositorio = new Dominio.Repositorio.ProdutosRepositorio();
+            var produtos = _repositorio.Produtos.Take(10); // Take seria o Top do Sql, retornará os 10 primeiros produtos
+
+            return View(produtos);
+        }
+    }
+}
