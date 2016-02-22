ceaa60c78117318f74f369c9d3dadebab0e23918
Leandro Passos <lpkyrius@hotmail.com>
2016-02-22 17:46:17 -0300

    Corrigido problema com string de conexão

--- Quiron.LojaVirtual.Web/Controllers/ProdutosController.cs
+++ Quiron.LojaVirtual.Web/Controllers/ProdutosController.cs
@@ -3,18 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Quiron.LojaVirtual.Dominio.Repositorio;
 
 namespace Quiron.LojaVirtual.Web.Controllers
 {
     public class ProdutosController : Controller
     {
-        private Dominio.Repositorio.ProdutosRepositorio _repositorio;
 
-        // GET: Produtos
+        private ProdutosRepositorio _repositorio;
+
+
+        //[Route("produtos/obter")]
         public ActionResult Index()
         {
-            _repositorio = new Dominio.Repositorio.ProdutosRepositorio();
-            var produtos = _repositorio.Produtos.Take(10); // Take seria o Top do Sql, retornará os 10 primeiros produtos
+            _repositorio = new ProdutosRepositorio();
+            var produtos = _repositorio.Produtos.Take(3);
+
+
 
             return View(produtos);
         }
---
+++ Quiron.LojaVirtual.Web/Controllers/VitrineController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Quiron.LojaVirtual.Dominio.Repositorio;
+
+namespace Quiron.LojaVirtual.Web.Controllers
+{
+    public class VitrineController : Controller
+    {
+        private Dominio.Repositorio.ProdutosRepositorio _repositorio;
+        public int ProdutosPorPagina = 3; // Pode ser um parâmetro ou por o valor no webconfig
+        // GET: Vitrine
+        public ActionResult ListaProdutos(int Pagina = 1)
+        {
+            _repositorio = new Dominio.Repositorio.ProdutosRepositorio();
+            var produtos = _repositorio.Produtos
+                .OrderBy(p => p.Descricao)
+                .Skip((Pagina - 1)*ProdutosPorPagina) // recebe a página que vai exibir, multiplca para pegar a quantidade (por pagina) que deve descosiderar pois já foram exibidos nas paginas anteriores
+                .Take(ProdutosPorPagina); //feito o skip dos registros exibidos nas paginas anteriores, agora pega os n produtos que devem ser exibidos nesta pagina (atual)
+
+            return View(produtos);
+        }
+    }
+}
